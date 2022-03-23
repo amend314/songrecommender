496040abf0956e833a0fc27467c05844ef02d42f
Armando Mendoza <amedoza935@gmail.com>
2022-03-23 12:09:44 -0500

    Setup some data, need to do more later

--- collaborativefilter.py
+++ collaborativefilter.py
@@ -2,4 +2,17 @@ import pandas as pd
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
+import os
+from sklearn.model_selection import train_test_split
+
+
+csvList = []
+
+for files in os.listdir('data'):
+    df = pd.read_csv('data/' + files, sep=',', header=0)
+    csvList.append(df)
+
+df = pd.concat(csvList, axis=0, ignore_index=True)
+df = df.drop(columns=['artist_name', 'track_name'])
+df_train, df_test = train_test_split(df, test_size=0.2)
 
